994ba22cda7bc60f950a2b9b69f235fc54a191de
Fabio Penteado <flzpenteado@gmail.com>
2022-04-19 09:19:50 -0300

    chore: added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayerModule } from './player/player.module';
 import { PlayerService } from './player/player.service';
 
+const DB_URL =
+  'mongodb+srv://admin:gRBEND7MEy6m3VND@cluster0.ihlye.mongodb.net/smartRanking?retryWrites=true&w=majority';
+
 @Module({
-  imports: [PlayerModule],
+  imports: [MongooseModule.forRoot(DB_URL), PlayerModule],
   controllers: [],
   providers: [PlayerService],
 })
